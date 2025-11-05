085dc28c09bdd231b479cbec1d5205298b231029
Eric English <eric.english@gmail.com>
2025-11-04 20:20:19 -0500

    Initial Commit

---
+++ littlelemon/Models/MenuItem.swift
@@ -0,0 +1,25 @@
+//
+//  MenuItem.swift
+//  littlelemon
+//
+//  Created by Eric English on 11/4/2025
+//
+
+import Foundation
+
+struct MenuItem: Codable, Identifiable {
+    var id = UUID()
+    let title : String
+    let descriptionDish: String
+    let price: String
+    let image: String
+    let category: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case descriptionDish = "description"
+        case price = "price"
+        case image = "image"
+        case category = "category"
+    }
+}
---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,21 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Eric English on 11/4/2025
+//
+
+import SwiftUI
+
+@main
+struct lLemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            Onboarding()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
+
